5678585fa7579ec614fabb48e24943bf9d7cd2cc
Brandon Closson <bclosson@gmail.com>
2014-05-06 13:40:03 -0400

    added cart and orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Betastore::Application.routes.draw do
-  root :to => 'products#index'
 
   get '/welcome' => 'static_pages#welcome'
-
-  resources :customers
-  get '/signup'        => 'customers#new', as: 'signup'
-  post '/signup'       => 'customers#create'
-
-  get '/login' => 'logins#new', as: 'login'
-  post '/login' => 'logins#create'
-  get    '/login' => 'logins#new',     as: 'login'
-  post   '/login' => 'logins#create'
-  delete '/logout' => 'logins#destroy', as: 'logout'
-  get '/verify/:token' => 'customers#verify', as: 'verify_customer'
-
-  resources :products
 
   namespace :admin do
     resources :products
@@ -22,4 +8,18 @@ Betastore::Application.routes.draw do
     post '/login' => 'logins#create'
   end
 
+  resources :customers
+  get '/signup'        => 'customers#new', as: 'signup'
+  post '/signup'       => 'customers#create'
+
+  get    '/login' => 'logins#new',     as: 'login'
+  post   '/login' => 'logins#create'
+  delete '/logout' => 'logins#destroy', as: 'logout'
+  get '/verify/:token' => 'customers#verify', as: 'verify_customer'
+
+  resources :products
+  resources :orders
+  resource :cart, only: [:update, :show, :delete]
+  post '/products/:product_id/add_to_cart' => 'carts#create', as: 'add_product_to_cart'
+  post '/products/:product_id/remove_from_cart' => 'carts#destroy', as: 'remove_product_from_cart'
 end
